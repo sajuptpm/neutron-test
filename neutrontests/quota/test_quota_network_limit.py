a99170a422b0e42b556f02f2e688994fb6b6741c
Saju Madhavan <sajuptpm@gmail.com>
2015-05-06 19:35:23 +0530

    use id to delete resouurce

--- neutrontests/quota/test_quota_network_limit.py
+++ neutrontests/quota/test_quota_network_limit.py
@@ -12,26 +12,25 @@ import quota_base
 class TestQuotaNetworkLimit(quota_base.TestQuotaBase):
 
 	def _init(self):	
-		self.network_names = []
+		self.network_ids = []
 		self.quota_limit = self._get_quota_limit('network')
 
 	def test_quota_network_limit(self):		
 		while True:
 			network_name = uuid.uuid4().get_hex()
 			network = {'name': network_name, 'admin_state_up': True}
-			self.neutron.create_network({'network':network})
-			self.logger.info("Created network:{0}".format(network_name))
-			self.network_names.append(network_name)
+			network_info = self.neutron.create_network({'network':network})
+			network_id = network_info['network']['id']
+			self.logger.info("Created network:{0}".format(network_id))
+			self.network_ids.append(network_id)
 			if not self.quota_limit:
 				break
 			self.quota_limit -= 1
 
 	def tearDown(self):
-		for network_name in self.network_names:
-			networks = self.neutron.list_networks(name=network_name)
-			if networks:
-				self.neutron.delete_network(networks['networks'][0]['id'])
-				self.logger.info("Deleted network:{0}".format(network_name))
+		for network_id in self.network_ids:
+			self.neutron.delete_network(network_id)
+			self.logger.info("Deleted network:{0}".format(network_id))
 
 
 if __name__ == '__main__':
